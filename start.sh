af71485ff5011dbe9bfddc1523c7a89471a9eaf4
SSSSSSL <dltmdtn116@naver.com>
2022-11-09 16:12:59 +0900

    docker-compose.yml -> docker stack & swarm mode

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-docker run --rm --name sitl-proxy -p 12345:12345 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf nginx
+# load balancing by nginx
+#docker run --rm -it --net=sitl_net --name sitl-proxy -p 12345:12345 -v $(pwd)/nginx.conf.with_net_alias:/etc/nginx/nginx.conf nginx
+#docker run --rm -it --name sitl-proxy -p 12345:12345 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf nginx
+
+# load balancing by docker-overlay
+docker stack deploy --compose-file docker-compose.yml sitl
